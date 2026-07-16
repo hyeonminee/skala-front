442794043acc4c2b6da8a202b26552ba35ebb0e6
박현민 <hhyeonmin.park@gmail.com>
2026-07-16 20:47:30 +0900

    7/16 index.html, weatherAPI.js 수정

--- script/weatherAPI.js
+++ script/weatherAPI.js
@@ -1,11 +1,13 @@
 // API 호출만 담당
 // 여기서 fetch() -> Open-Meteo 서버
 
+// 외부에서 가져다 쓸 수 있도록 export를 함수 맨 앞에 붙인다.
 export async function getWeather(lat, lon) {
     const url = `https://api.open-meteo.com/v1/forecast?latitude=${lat}&longitude=${lon}&current=temperature_2m,relative_humidity_2m`;
     const response = await fetch(url);
     const data = await response.json();
 
+    // 필요한 데이터만 객체로 패킹해서 리턴
     return {
         temperature: data.current.temperature_2m,
         humidity: data.current.relative_humidity_2m
